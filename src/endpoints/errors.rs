c8475506719a5a0784aebdc66683b8ba5ce2b42b
Ameya Bhat <ameyabhat1918@gmail.com>
2022-09-11 20:03:59 -0400

    Add get_applicant(s) endpoint + clean up error handling. Warp keeps returning 405's instead of 404's - I should fix this in warp at some point

--- src/endpoints/errors.rs
+++ src/endpoints/errors.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use warp::reject;
 
+use crate::model::types::Applicant;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ApiError {
     DuplicateUser,
@@ -11,6 +13,11 @@ pub enum ApiError {
         given_solution: HashMap<String, u64>,
     },
     DeserializeError,
+    ApplicantsNotFound {
+        applicants_found: Vec<Applicant>,
+        applicants_not_found: Vec<String>,
+    },
+    NoUserFound,
 }
 
 #[derive(thiserror::Error, Debug, Serialize, Deserialize)]
@@ -22,6 +29,15 @@ pub enum ModelError {
     },
     #[error("A registration with this NUID exists")]
     DuplicateUser,
+    #[error("One or more of the applicants requested not found")]
+    ApplicantsNotFound {
+        applicants_found: Vec<Applicant>,
+        applicants_not_found: Vec<String>,
+    },
+    #[error("SQL error")]
+    SqlError,
+    #[error("No user with this token exists")]
+    NoUserFound,
 }
 
 impl reject::Reject for ModelError {}
--- src/endpoints/server.rs
+++ src/endpoints/server.rs
@@ -1,4 +1,3 @@
-use core::panic;
 use std::collections::HashMap;
 use std::convert::Infallible;
 
@@ -7,16 +6,19 @@ use super::messages::{
     ErrorResponse, GetChallengeString, HandleForgotTokenResponse, RegisterRequest, RegisterResponse,
 };
 use super::routes::{
-    forgot_token_route, get_applicant_route, get_challenge_string_route, health, register_route,
-    submit, with_db,
+    forgot_token_route, get_applicant_route, get_applicants_route, get_challenge_string_route,
+    health, register_route, submit, with_db,
 };
 use crate::endpoints::ApiError;
-use crate::model::{self, check_solution, register_user, retreive_challenge, retreive_token};
+use crate::model::{
+    check_solution, get_applicants, register_user, retreive_challenge, retreive_token,
+};
 use serde_json::json;
 use sqlx::PgPool;
 use uuid::Uuid;
 use warp::body::BodyDeserializeError;
 use warp::hyper::StatusCode;
+use warp::reject::MethodNotAllowed;
 use warp::{reject, reply, Filter, Rejection, Reply};
 
 type WarpResponse = Result<impl Reply, Rejection>;
@@ -61,22 +63,73 @@ pub fn end(o: Option<PgPool>) -> impl Filter<Extract = impl Reply, Error = Infal
             handle_get_challenge
         ))
         .or(health().and_then(health_check))
-        /*         .or(handle_with_db!(
+        .or(handle_with_db!(
             get_applicant_route,
             o,
             handle_get_applicant
-        )) */
+        ))
+        .or(handle_with_db!(
+            get_applicants_route,
+            o,
+            handle_get_applicants
+        ))
         .recover(handle_rejection)
 }
-/*
-pub async fn handle_get_applicant(nuid: String, p: PgPool) -> WarpResponse {
+
+// This is weird - if I use the WarpResult alias here, it forces me to use the same
+// concrete return types as the other functions that use the WarpResult alias
+// def because using `impl trait` syntax in aliases is experimental and on nightly
+// should switch away from nightly - it'll make deployment more stable as well
+pub async fn handle_get_applicant(nuid: String, p: PgPool) -> Result<impl Reply, Rejection> {
     // look up the applicant
-    let applicants = match get_applicants(vec![nuid]).await {
-        Ok(_) => todo!(),
-        Err(_) => todo!(),
+    match get_applicants(p, &[nuid]).await {
+        Ok(applicant) => {
+            let code;
+            if applicant.len() == 1 {
+                code = StatusCode::OK;
+                Ok(reply::with_status(reply::json(&applicant[0]), code))
+            } else if applicant.is_empty() {
+                code = StatusCode::NOT_FOUND;
+                Ok(reply::with_status(
+                    reply::json(&"Could not find submission from applicant"),
+                    code,
+                ))
+            } else {
+                let code = StatusCode::INTERNAL_SERVER_ERROR;
+                let msg = api_err!("Fetched the wrong number of applicants somehow ¯\\_(ツ)_/¯  ");
+                Ok(reply::with_status(reply::json(&msg), code))
+            }
+        }
+        // This will just bubble down to a 500 which seems super reasonable
+        // Assuming that this is a sql error - no other reason that this would fail
+        Err(_) => Err(reject::custom(ModelError::SqlError)),
     }
 }
-*/
+
+pub async fn handle_get_applicants(nuids: Vec<String>, p: PgPool) -> WarpResponse {
+    match get_applicants(p, &nuids).await {
+        Ok(applicants) => {
+            if applicants.len() == nuids.len() {
+                Ok(reply::json(&applicants))
+            } else {
+                let mut applicants_not_found: Vec<String> = nuids.clone();
+                // ok so basically we copy the nuids to a new list,
+                // then keep only the nuids that do not correspond to any of the
+                // applicants we fetched. This means that we only get the applicants
+                // that aren't found
+                // worst case O((n/2)^2) - polynomial so we're fine
+                applicants_not_found
+                    .retain(|nuid| !applicants.iter().any(|applicant| applicant.nuid == *nuid));
+
+                Err(reject::custom(ModelError::ApplicantsNotFound {
+                    applicants_found: applicants,
+                    applicants_not_found,
+                }))
+            }
+        }
+        Err(_) => todo!("falling through here"),
+    }
+}
 
 pub async fn handle_register(request: RegisterRequest, p: PgPool) -> WarpResponse {
     info!(
@@ -91,7 +144,7 @@ pub async fn handle_register(request: RegisterRequest, p: PgPool) -> WarpRespons
         })),
         // Actually send back an error here you fucking muppet
         // Should be a 409 conflict error if the error doesnt exist,
-        Err(_) => Err(reject::custom(ModelError::DuplicateUser)),
+        Err(e) => Err(reject::custom(e)),
     }
 }
 
@@ -110,17 +163,17 @@ pub async fn handle_submit(token: Uuid, soln: HashMap<String, u64>, p: PgPool) -
                 }))
             }
         }
-        Err(_) => panic!("unclear on how we get to this - either we failed to deserialize the soln, or the db conn fails. Regardless, log it and send back a 500"),
+        Err(e) => Err(reject::custom(e)),
     }
 }
 
 pub async fn handle_forgot_token(nuid: String, p: PgPool) -> Result<impl Reply, Rejection> {
-    let token = match retreive_token(p, nuid).await {
-        Ok(token) => token.to_string(),
-        Err(_e) => todo!("send back no user found failure"),
-    };
-
-    Ok(reply::json(&HandleForgotTokenResponse { token }))
+    match retreive_token(p, nuid).await {
+        Ok(token) => Ok(reply::json(&HandleForgotTokenResponse {
+            token: token.to_string(),
+        })),
+        Err(e) => Err(reject::custom(e)),
+    }
 }
 
 pub async fn health_check() -> WarpResponse {
@@ -132,7 +185,7 @@ pub async fn health_check() -> WarpResponse {
 pub async fn handle_get_challenge(token: Uuid, pool: PgPool) -> WarpResponse {
     match retreive_challenge(&pool, token).await {
         Ok(challenge_string) => Ok(reply::json(&GetChallengeString { challenge_string })),
-        Err(_) => todo!(),
+        Err(e) => Err(reject::custom(e)),
     }
 }
 
@@ -140,16 +193,7 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
     let code;
     let msg: ErrorResponse;
 
-    // This is super jank - we're mapping a 405 to a 404
-    // This issue explains why: https://github.com/seanmonstar/warp/issues/77
-    // I'll fix this eventually, I need to fix the library
-    // fucking warp man
-    if err.find::<warp::reject::MethodNotAllowed>().is_some() {
-        code = StatusCode::NOT_FOUND;
-        msg = api_err!(
-            "The path you're trying to hit doesn't exist - check your endpoints and your request method"
-        );
-    } else if let Some(wrapped_err) = err.find::<ModelError>() {
+    if let Some(wrapped_err) = err.find::<ModelError>() {
         match wrapped_err {
             ModelError::DuplicateUser => {
                 msg = api_err!("This NUID has already been used to register");
@@ -168,10 +212,44 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
                 );
                 code = StatusCode::BAD_REQUEST;
             }
+            ModelError::ApplicantsNotFound {
+                applicants_found,
+                applicants_not_found,
+            } => {
+                code = StatusCode::NOT_FOUND;
+                msg = api_err!(
+                    "Submissions from one or more the applicants requested was not found",
+                    ApiError::ApplicantsNotFound {
+                        applicants_found: applicants_found.to_vec(),
+                        applicants_not_found: applicants_not_found.clone()
+                    }
+                )
+            }
+            ModelError::SqlError => {
+                code = StatusCode::INTERNAL_SERVER_ERROR;
+                msg = api_err!("SQL Error - text me if this happens");
+                warn!("{:?}", err)
+            }
+            ModelError::NoUserFound => {
+                code = StatusCode::NOT_FOUND;
+                msg = api_err!("No user with this token or nuid exists")
+            }
         }
     } else if err.find::<BodyDeserializeError>().is_some() {
         code = StatusCode::BAD_REQUEST;
         msg = api_err!("Bad request - check your request body")
+    }
+    // This is super jank - we're mapping a 405 to a 404
+    // This issue explains why: https://github.com/seanmonstar/warp/issues/77
+    // I'll fix this eventually, I need to fix the library
+    // fucking warp man
+    // This shit sucks - for some reason post request are being logged as
+    // methodNotAllowed
+    else if err.find::<MethodNotAllowed>().is_some() {
+        code = StatusCode::NOT_FOUND;
+        msg = api_err!(
+            "The path you're trying to hit doesn't exist - check your endpoints and your request method"
+        );
     } else {
         code = StatusCode::INTERNAL_SERVER_ERROR;
         msg = api_err!("UNHANDLED_REJECTION");
@@ -185,5 +263,5 @@ mod tests {
     // We'll write API tests here eventually - i wonder if I can write service tests
     // using docker compose. Should probably figure out how to do that for generate
 
-    // You can just mock the DB you muppet
+    // You can just mock the DB
 }
